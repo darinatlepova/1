5897b13ff276b218cdb90dc0db955cf45ac84c11
darinatlepova <darina.tlepova@gmail.com>
2026-01-31 18:52:32 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.config import CORS_ORIGINS
+from backend.app.config import CORS_ORIGINS
 from app.database import init_db
 from app.api import auth, tasks
 
