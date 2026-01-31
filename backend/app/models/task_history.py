88a97b56b6bcbffae2aa94c7952cf168c6fe69b5
darinatlepova <darina.tlepova@gmail.com>
2026-01-31 20:25:33 +0500

    Update task_history.py

--- backend/app/models/task_history.py
+++ backend/app/models/task_history.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum as SQ
 from sqlalchemy.orm import relationship
 import enum
 
-from app.database import Base
+from backend.app.database import Base
 
 
 class TaskHistoryChangeType(str, enum.Enum):
